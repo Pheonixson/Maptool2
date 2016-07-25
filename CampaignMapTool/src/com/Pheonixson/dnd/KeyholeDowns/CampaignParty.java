f2badc46eea37b61fcce532e8d3c197d8feaac7c
andrew.nixon <andrew.nixon@in4le0049748mac.sats.corp>
2016-07-25 10:23:39 -0600

    added party goal to CampaignParty

--- CampaignMapTool/src/com/Pheonixson/dnd/KeyholeDowns/CampaignParty.java
+++ CampaignMapTool/src/com/Pheonixson/dnd/KeyholeDowns/CampaignParty.java
@@ -3,10 +3,23 @@ package com.Pheonixson.dnd.KeyholeDowns;
 public class CampaignParty {
 	private CampaignLocation partyLocation;
 	private String partyMembers;
+	private String partyGoal;
 
 	public CampaignParty() {
 		this.partyLocation = new CampaignLocation();
 		this.partyMembers = "Knights of Placeholder";
+		this.partyGoal = "Not die";
+	}
+
+	/**
+	 * @param partyLocation
+	 * @param partyMembers
+	 * @param partyGoal
+	 */
+	public CampaignParty(CampaignLocation partyLocation, String partyMembers, String partyGoal) {
+		this.partyLocation = partyLocation;
+		this.partyMembers = partyMembers;
+		this.partyGoal = partyGoal;
 	}
 
 	/**
@@ -46,6 +59,21 @@ public class CampaignParty {
 	 */
 	public void setPartyMembers(String partyMembers) {
 		this.partyMembers = partyMembers;
+	}
+
+	/**
+	 * @return the partyGoal
+	 */
+	public String getPartyGoal() {
+		return partyGoal;
+	}
+
+	/**
+	 * @param partyGoal
+	 *            the partyGoal to set
+	 */
+	public void setPartyGoal(String partyGoal) {
+		this.partyGoal = partyGoal;
 	}
 
 }
